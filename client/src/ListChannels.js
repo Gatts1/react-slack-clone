8a0cd8e6e12dc3421a891c4ee203bd7a9d964ccf
Diego Cuevas <diegocuevas579@gmail.com>
2019-05-30 18:57:40 -0500

    Fix bug

--- client/src/ListChannels.js
+++ client/src/ListChannels.js
@@ -3,13 +3,6 @@ import Channel from "./Channel";
 import { link } from "fs";
 
 function ListChannels() {
-
-  const channelsFakeData = [
-    {name: "General", id: 1},
-    {name: "Varios", id: 2},
-    {name: "Forobardo", id: 3},
-    {name: "", id: 4}
-  ]
   return <Channel />;
 }
 
