dcf3bdabd554290dcec0c1117e5e66afc0e2b602
Cristian Granda <cristian.granda.pastor@gmail.com>
2019-05-30 17:25:52 -0500

    Add base to the project (#3)

---
+++ client/src/ChannelsSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AddChannel from "./AddChannel";
+import ListChannels from "./ListChannels";
+
+function ChannelsSection() {
+  return (
+    <>
+      <AddChannel />
+      <ListChannels />
+    </>
+  );
+}
+
+export default ChannelsSection;
