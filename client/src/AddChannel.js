b190b4bedb98cc605ac0dcefd880a368d743369f
Diego Cuevas <DiegoCuevas@users.noreply.github.com>
2019-05-31 09:39:26 -0500

    Add components to channels (#4)
    * Add components to channels
    
    * Fix bug

--- client/src/AddChannel.js
+++ client/src/AddChannel.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React from 'react';
 
 function AddChannel() {
-  return <h1>AddChannel</h1>;
+  const [showChannel, setShowChannel] = React.useState(false);
+  function handleOnClick() {
+    setShowChannel(true);
+  }
+  function handleOnSubmit(event) {
+    event.preventDefault();
+    setShowChannel(false);
+  }
+  return (
+    <>
+      <h2>Channels</h2>
+      <button type="button" onClick={handleOnClick}>
+        +
+      </button>
+      {showChannel && (
+        <div>
+          <form action="" onSubmit={handleOnSubmit} onReset={handleOnSubmit}>
+            <p>Create Channel</p>
+            <p>
+              Channels are where your members communicate. They’re best when
+              organized around a topic — #leads, for example.
+            </p>
+            <label htmlFor="createChannel">Name</label>
+            <input type="text" id="createChannel" />
+            <button type="submit">Create Channel</button>
+            <button type="reset">Cancel</button>
+          </form>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default AddChannel;
