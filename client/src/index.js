a33d8665e6c15a223bcd92cf6451271efbe98006
Brayan Ciudad <linzeur@gmail.com>
2019-05-31 16:54:10 -0500

    Merge with master

--- client/src/index.js
+++ client/src/index.js
@@ -8,16 +8,6 @@ import { render } from "react-dom";
 // import Header from "./header-channel";
 import Date from "./message-separator-date";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  fas,
-  faStar,
-  faUser,
-  faThumbtack
-} from "@fortawesome/free-solid-svg-icons";
-
-library.add(fas, faStar, faUser, faThumbtack);
-
 const $root = document.getElementById("root");
 
 render(<Date date="05/31/2019" />, $root);
