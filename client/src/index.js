c9bc9581f08da081515af5230418de20b79adba9
cristianbgp <cristian.granda.pastor@gmail.com>
2019-05-31 17:27:40 -0500

    Solve conflicts with master

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
 
-// import App from "./App";
-
-// const $root = document.getElementById("root");
-// render(<App url="ws://localhost:4000" />, $root);
-// import Header from "./header-channel";
-import Date from "./message-separator-date";
+import App from "./App";
 
 const $root = document.getElementById("root");
+render(<App url="ws://localhost:4000" />, $root);
+// import Header from "./header-channel";
+// import Date from "./message-separator-date";
 
-render(<Date date="05/31/2019" />, $root);
+// const $root = document.getElementById("root");
+
+// render(<Date date="05/31/2019" />, $root);
