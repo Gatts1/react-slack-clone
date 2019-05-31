a7d6b8d49c9ead628e561d9d6fb84db46f8b7dbb
Carlos A <cayala.w@gmail.com>
2019-05-31 17:17:38 -0500

    Merge with master

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "./App";
-import Header from "./header-channel";
-import ChannelElements from "./channel-element";
+
+// import App from "./App";
 
 // const $root = document.getElementById("root");
 // render(<App url="ws://localhost:4000" />, $root);
+// import Header from "./header-channel";
+import Date from "./message-separator-date";
 
 const $root = document.getElementById("root");
 
-render(<ChannelElements />, $root);
+render(<Date date="05/31/2019" />, $root);
