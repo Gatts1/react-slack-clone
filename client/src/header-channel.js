d71baa18896180e2b2dc830d2bef16cb0dc553b2
Carlos A <cayala.w@gmail.com>
2019-05-30 20:39:36 -0500

    Correct conflicts left after last merge

--- client/src/header-channel.js
+++ client/src/header-channel.js
@@ -1,5 +1,6 @@
 /** @jsx jsx */
-// import React from "react";
+
+import React from "react";
 import { jsx } from "@emotion/core";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
