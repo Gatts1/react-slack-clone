9532c83fdf4bdf79a994981bd8437bdbdd9d123d
Brayan Ciudad <linzeur@gmail.com>
2019-05-30 19:44:11 -0500

    Add CSS in JS left side

--- client/src/header-channel.js
+++ client/src/header-channel.js
@@ -2,6 +2,8 @@
 import React from "react";
 import { jsx } from "@emotion/core";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function HeaderChannel({ name }) {
   return (
     <div
@@ -16,14 +18,49 @@ function HeaderChannel({ name }) {
         borderBottom: "1px solid lightgray"
       }}
     >
-      <div class="channel-info">
-        <i id="hamburguer" class="fas fa-bars" />
-        <p class="channel-title" id="channel-title">
+      <div
+        css={{
+          marginTop: 10,
+          marginLeft: 18
+        }}
+      >
+        <p
+          id="channel-title"
+          css={{
+            fontWeight: "bolder",
+            fontSize: 18,
+            marginBottom: 2,
+            paddingLeft: 5
+          }}
+        >
           #general
         </p>
-        <div class="quick-options-and-topics">
-          <i class="far fa-star" /> | <i class="far fa-user" /> 0 |
-          <i class="fas fa-thumbtack" /> 0 | Add topic
+        <div
+          css={{
+            fontSize: 13,
+            color: "#4d4d4d",
+            visibility: "visible"
+          }}
+        >
+          <FontAwesomeIcon
+            icon="star"
+            css={{
+              color: "white",
+              strokeWidth: "20",
+              stroke: "black"
+            }}
+          />{" "}
+          |{" "}
+          <FontAwesomeIcon
+            icon="user"
+            css={{
+              color: "white",
+              strokeWidth: "20",
+              stroke: "black"
+            }}
+          />{" "}
+          0 |
+          <FontAwesomeIcon icon="thumbtack" /> 0 | Add topic
         </div>
       </div>
       <div class="channel-options">
--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,16 @@ import App from "./App";
 // render(<App url="ws://localhost:4000" />, $root);
 import Header from "./header-channel";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  fas,
+  faStar,
+  faUser,
+  faThumbtack
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(fas, faStar, faUser, faThumbtack);
+
 const $root = document.getElementById("root");
 
 render(<Header />, $root);
