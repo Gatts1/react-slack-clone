51e25c46e33afb6ea98f7b4b715d1ec04f524c20
Brayan Ciudad <linzeur@gmail.com>
2019-05-31 21:42:40 -0500

    Create component right side

--- client/src/right-side.js
+++ client/src/right-side.js
@@ -2,14 +2,21 @@ import React from "react";
 import { jsx } from "@emotion/core";
 
 import HeaderChannel from "./header-channel";
-import ChannelElement from "./channel-element";
 
 function RightSide() {
   return (
-    <>
+    <div
+      css={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        justifyContent: "space-between",
+        alignItems: "center",
+        flexGrow: 1
+      }}
+    >
       <HeaderChannel />
-      <ChannelElement />
-    </>
+    </div>
   );
 }
 
