bf5cbf71e5fc0fff3a303747fbacb8d99a2c7d6a
Cristian Granda <cristian.granda.pastor@gmail.com>
2019-06-01 16:25:53 -0500

    Add styles to the LeftSide

--- client/src/channel-element.js
+++ client/src/channel-element.js
@@ -1,14 +1,32 @@
+/** @jsx jsx */
 import React from "react";
 import { jsx } from "@emotion/core";
-
-import MessageBody from "./message-body";
 
 function ChannelElement({ channelName }) {
   function handleClick(event) {
     console.log("Add channel", channelName);
   }
 
-  return <li onClick={handleClick}># {channelName}</li>;
+  return (
+    <li
+      onClick={handleClick}
+      css={{
+        boxSizing: "border-box",
+        width: "100%",
+        paddingTop: "3px",
+        paddingBottom: "3px",
+        paddingLeft: "10px",
+        listStyle: "none",
+        color: "rgb(167, 163, 168)",
+        "&:hover": {
+          backgroundColor: "#301231",
+          cursor: "pointer"
+        }
+      }}
+    >
+      # {channelName}
+    </li>
+  );
 }
 
 export default ChannelElement;
