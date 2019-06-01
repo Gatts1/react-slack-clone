bfea8c1115fec166b28b9150eac39eb0d781ef91
Cristian Granda <cristian.granda.pastor@gmail.com>
2019-06-01 17:00:37 -0500

    Add styles to the LeftSide (#15)
    * Add styles to the LeftSide
    
    * Add little change

--- client/src/channel-list.js
+++ client/src/channel-list.js
@@ -1,11 +1,20 @@
+/** @jsx jsx */
 import React from "react";
+import { jsx } from "@emotion/core";
 import ChannelElement from "./channel-element";
 
 function ChannelList({ listChannel }) {
   const channelsData = listChannel;
 
   return (
-    <ul>
+    <ul
+      css={{
+        paddingLeft: "0",
+        width: "100%",
+        margin: "0",
+        marginBottom: "10px"
+      }}
+    >
       {channelsData.map(channel => {
         return <ChannelElement key={channel.id} channelName={channel.name} />;
       })}
