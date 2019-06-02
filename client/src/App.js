aa71b62c8663b86adbee211a26dc712836481e37
Brayan Ciudad <linzeur@gmail.com>
2019-06-01 23:08:40 -0500

    Revise bugs

--- client/src/App.js
+++ client/src/App.js
@@ -10,15 +10,7 @@ function App({ url }) {
   const [username, setUsername] = React.useState(null);
 
   const [listChannel, setListChannel] = React.useState([
-<<<<<<< HEAD
-<<<<<<< HEAD
     { id: Date.now(), name: "general", messages: [] }
-=======
-    { name: "general", id: Date.now() }
->>>>>>> Add property to message object
-=======
-    { id: Date.now(), name: "general", messages: [] }
->>>>>>> Join a new channel and change amoung them
   ]);
 
   const [listMessages, setListMessages] = React.useState([]);
@@ -52,97 +44,36 @@ function App({ url }) {
         ws.current = new WebSocket(url);
       };
       ws.current.onmessage = ({ data }) => {
-        console.log("data", data);
-        const messageReceive = JSON.parse(data);
+        let messageReceive = JSON.parse(data);
 
         if (messageReceive.hasOwnProperty("newChannel")) {
-          receiveNewChannel(messageReceive);
-          return;
-        }
-
-        if (
+          receiveNewChannel(messageReceive["newChannel"]);
+        } else if (
           messageReceive.hasOwnProperty("channel") &&
           messageReceive.hasOwnProperty("body")
         ) {
           receiveNewMessage(messageReceive);
-          return;
         }
       };
     }
   }, [ws.current]);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
   React.useEffect(() => {
     setListMessages(listChannel[indexChannelActive]["messages"]);
   }, [indexChannelActive]);
 
-=======
->>>>>>> Add setIndexChannelActive functionality
-=======
->>>>>>> Advances
-=======
-  React.useEffect(() => {
-    console.log("23");
-    setListMessages(listChannel[indexChannelActive]["messages"]);
-  }, [indexChannelActive]);
-
->>>>>>> Add little useEffect
   function submitSendChannel(channelName) {
     const newChannel = { id: Date.now(), name: channelName, messages: [] };
     if (connected) ws.current.send(JSON.stringify({ newChannel: newChannel }));
   }
 
   function submitSendMessage(messageContent) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Join a new channel and change amoung them
-    let newMessage = {
-=======
     const newMessage = {
->>>>>>> Fix bugs
       id: new Date().toISOString(),
       author: username,
       content: messageContent,
       date: new Date().toISOString()
     };
-<<<<<<< HEAD
-    setListMessages(listMessages.concat(newMessage));
-    listChannel[indexChannelActive]["messages"].push(newMessage);
-<<<<<<< HEAD
-=======
-    setListMessages(
-      listMessages.concat({
-        id: new Date().toISOString(),
-        author: username,
-        content: messageContent,
-        date: new Date().toISOString(),
-        indexChannel: indexChannelActive
-      })
-    );
-<<<<<<< HEAD
-<<<<<<< HEAD
-    setListChannel();
->>>>>>> Advances
-=======
-    console.log(listChannel);
->>>>>>> Add property to message object
-=======
->>>>>>> Add little useEffect
-=======
->>>>>>> Join a new channel and change amoung them
-    ws.current.send(
-      JSON.stringify({
-        id: new Date().toISOString(),
-        author: username,
-        content: messageContent,
-        date: new Date().toISOString()
-      })
-    );
-=======
     if (connected)
       ws.current.send(
         JSON.stringify({
@@ -150,27 +81,20 @@ function App({ url }) {
           body: newMessage
         })
       );
->>>>>>> Fix bugs
   }
-
-  function changeActiveChannel(newId) {
-    setIndexChannelActive(newId);
-  }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> Add setIndexChannelActive functionality
-=======
->>>>>>> Join a new channel and change amoung them
-=======
 
   function receiveNewChannel(channel) {
-    setListChannel(listChannel.concat(channel["newChannel"]));
+    console.log("prueba");
+    console.log(channel);
+    console.log(listChannel.concat(channel));
+    setListChannel(listChannel.concat(channel));
+    console.log(listChannel);
   }
 
   function receiveNewMessage(message) {
+    console.log(message);
+    console.log(indexChannelActive);
+    console.log(listChannel);
     if (message["channel"] === listChannel[indexChannelActive]["name"]) {
       setListMessages(listMessages.concat(message["body"]));
       listChannel[indexChannelActive]["messages"].push(message["body"]);
@@ -184,7 +108,6 @@ function App({ url }) {
     }
   }
 
->>>>>>> Fix bugs
   return (
     <>
       {isLogged ? (
@@ -194,14 +117,7 @@ function App({ url }) {
           submitSendMessage={submitSendMessage}
           listMessages={listMessages}
           username={username}
-<<<<<<< HEAD
-<<<<<<< HEAD
           channelName={listChannel[indexChannelActive]["name"]}
-=======
->>>>>>> Add setIndexChannelActive functionality
-=======
-          channelName={listChannel[indexChannelActive]["name"]}
->>>>>>> Join a new channel and change amoung them
           setIndexChannelActive={setIndexChannelActive}
         />
       ) : (
